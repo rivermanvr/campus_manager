2b0f0ce42aa369229755d60a315d1b772bcd96b3
rivermanvr <vrgithub@riversconsulting.com>
2017-09-30 14:35:16 -0400

    refactored reducers folder and files

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import loggerMiddleware from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import axios from 'axios';
-import reducers from './reducers';
+import reducers from '../reducers';
 
 
 const store = createStore(
